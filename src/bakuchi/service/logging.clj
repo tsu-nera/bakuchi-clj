29b444fa18dd4f146a564ccc9e7da2f5bb88458b
tsu-nera <fox10225fox@gmail.com>
2022-07-25 17:19:25 +0900

    :wrench:ログ表示改善

--- src/bakuchi/service/logging.clj
+++ src/bakuchi/service/logging.clj
@@ -1,9 +1,20 @@
 (ns bakuchi.service.logging
   (:require
+   [clojure.tools.logging :as log]
    [integrant.core :as ig]
    [unilog.config  :refer [start-logging!]]))
 
-(def config-map {:level "info" :console true})
+(def config-map
+  {:level     "info"
+   :console   false
+   :appenders [{:appender :console
+                :encoder  :pattern
+                :pattern  "[%d] %p - %m%n"}]})
 
 (defmethod ig/init-key ::logger [_ _]
   (start-logging! config-map))
+
+(comment
+  (require '[clojure.tools.logging :as log])
+  (log/info "test")
+  )
